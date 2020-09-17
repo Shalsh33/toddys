d142b1ea22bd300cd12df8f777b057fd69c8664d
Blas <blas.e.martinez@live.com>
2020-09-17 09:56:50 -0300

    agregado carpetas

--- router - copia.php
+++ router - copia.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'modulos/tabla.php';
 
 // defino la base url para la construccion de links con urls semánticas
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
@@ -7,7 +8,7 @@ define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] 
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 } else {
-    $action = 'admin'; // acción por defecto si no envían
+    $action = 'tabla'; // acción por defecto si no envían
 }
 
 // parsea la accion Ej: suma/1/2 --> ['suma', 1, 2]
@@ -15,17 +16,12 @@ $params = explode('/', $action);
 
 // determina que camino seguir según la acción
 switch ($params[0]) {
-    case 'admin':
-        require_once 'app/controller/admin.controller.php';
-		$controller = new admin_controller();
-		if (empty($params[1])){
-			$controller->init();
+    case 'tabla':
+        if (!empty($params[1])){
+			tabla($params[1]);
 		}
 		else{
-			switch($params[1]){
-				case 'concejales':
-					$controller->listar_concejales();
-			}
+			tabla();
 		}
 		break;
     default:
---
+++ templates/header.php
@@ -0,0 +1,7 @@
+<html>
+	<head>
+		<base href="<?=BASE_URL?>">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title> Bloque de todos </title>
+	</head>
