b48c7d60664a631d2bf2371b021b853074e6e0b1
Blas Martínez <blas.e.martinez@live.com>
2020-09-18 15:20:38 -0300

    nav y footer creados

---
+++ templates/footer.php
@@ -0,0 +1,15 @@
+<footer>
+
+	<section id="logo">
+		
+		<a href="/inicio"> <img src="/includes/img/logo.png" alt="inicio" /></a>
+		
+	</section>
+	
+	<section id="developer">
+	
+		<p> Desarrollado por <span>BlasM</span> </p>
+
+	</section>
+
+</footer>
---
+++ templates/nav.php
@@ -0,0 +1,21 @@
+<header>
+	<nav id="navigation">
+		<section id="logo">
+			
+			<a href="/inicio"> <img src="/includes/img/logo.png" alt="inicio" /></a>
+			
+		</section>
+		
+		<section id="links">
+		
+			<ul id="nav-links">
+				
+				<a href="/inicio"><li>Inicio</li></a>
+				<a href="/comisiones"><li>Comisiones</li></a>
+				<a href="/login"><li>Soy miembro</li></a>
+				
+			</ul>
+		
+		</section>
+	</nav>
+</header>
