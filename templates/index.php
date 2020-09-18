6721a8359ce454c9be2181dd5af14c461d4e4890
Blas <blas.e.martinez@live.com>
2020-09-18 18:59:44 -0300

    primer versión de la pantalla principal

---
+++ templates/index.php
@@ -0,0 +1,9 @@
+<?php
+	
+	echo "<article id='main'>";
+	foreach($datos as $persona){
+		echo ($persona->presidente) ? "<section class='presidente'>" : "<section class='concejal'>";
+		echo "<img src='/includes/img/$persona->foto' alt='$persona->nombre'/>
+				<h2 class='nombre'>$persona->nombre</h2>
+				<p>$persona->descripcion</p>";
+	
