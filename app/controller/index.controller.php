7b8a25c62f896f9cab3c2af5d2503290c9a55f85
Blas Martínez <blas.e.martinez@live.com>
2020-09-19 15:13:22 -0300

    Creación index

---
+++ app/controller/index.controller.php
@@ -0,0 +1,28 @@
+<?php
+	
+require_once "app/model/model.personas.php";
+require_once "app/model/model.relaciones.php";
+require_once "app/view/index.view.php";
+	
+class index_controller {
+	
+	protected $view;
+	protected $model_personas;
+	protected $model_relaciones;
+	
+	function __construct(){
+		
+		$this -> view = new index_view();
+		$this -> model_personas = new model_personas();
+		$this -> model_relaciones = new model_relaciones();
+		
+	}
+	
+	function init(){
+		
+		$datos = $this -> model_personas -> obtener_personas();
+		$this -> view -> index($datos);
+			
+	}
+	
+}
