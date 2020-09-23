ccb1ce63972dc5d32519a3056497f5b35b058340
Blas Martínez <blas.e.martinez@live.com>
2020-09-23 13:56:40 -0300

    Correcciones al controller del admin

--- app/controller/admin.controller.php
+++ app/controller/admin.controller.php
@@ -21,28 +21,25 @@ class admin_controller{
 		$this->view->main_page();
 	}
 	
-	function listar_personas(){
+	function list_personas(){
 		$array_db = $this->model_personas->obtener_personas();
 		
 		$this->view->administrar_personas($array_db);
 	}
 	
-	function editar_persona($id){
+	function edit_persona($id){
 		
 		($id) ? $info = $this->model_personas->obtener_uno($id) : $info = null;
 		
 		($info) ? $this->view->edicion($info) : $this -> error_id();
 	}
 	
-	function enviar_edit($id) {
+	function send_edit($id) {
 		
-		if (isset($_POST['presidente'])){
-			$result = $this->model_personas->editar_persona($id, $_POST['nombre'], $_POST['periodo'], $_POST['descripcion'], true, $_POST['foto']);
-		} else {
-			$result = $this->model_personas->editar_persona($id, $_POST['nombre'], $_POST['periodo'], $_POST['descripcion'], false, $_POST['foto']);
-		}
+		(isset($_POST['presidente']) ? $result = $this->model_personas->editar_persona($id, $_POST['nombre'], $_POST['periodo'], $_POST['descripcion'], true, $_POST['foto']); 
+									 : $result = $this->model_personas->editar_persona($id, $_POST['nombre'], $_POST['periodo'], $_POST['descripcion'], false, $_POST['foto']);
 		
-		$this->listar_personas();
+		$this->list_personas();
 	}
 
 	
