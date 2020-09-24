9b8ee942e68c5a7efb0c13c08915f75fef3badc5
Blas Martínez <blas.e.martinez@live.com>
2020-09-24 10:10:04 -0300

    Avances y correcciones sobre el router

--- router.php
+++ router.php
@@ -24,7 +24,7 @@ switch ($params[0]) {
 		break;
 	case 'index':
 		$controller = new index_controller();
-		$controller -> init();
+		$controller->init();
 		break;
     default:
         echo('404 Page not found');
@@ -36,39 +36,74 @@ function case_admin ($params){
 	
 	if (empty($params[1])){ //si no tengo más parámetros
 		$controller->init(); //inicio la página
-	}
-	else{
+	} else{
 		
 		switch($params[1]){ //sino, veo a que tabla se quiere acceder
 		
 			case 'personas':
 				if(empty($params[2])){ //Si no tengo otro parámetro,
-					$controller->listar_personas();//listo los elementos en la tabla persona
+					$controller->list_personas();//listo los elementos en la tabla persona
 				} 
 				else{
 					switch($params[2]){ //sino, vamos a analizar la acción
 						case 'edit': 
-							(!empty($params[3])) ? $controller->editar_persona($params[3]) : $controller -> editar_persona(null); 
+							(!empty($params[3])) ? $controller->edit_persona($params[3]) : $controller->edit_persona(null); 
 							//interfaz de edición (necesito un 3er párametro, si no está seteado, null)
 							break;
-							
+						
 						case 'update':
-							$controller->enviar_edit($params[3]);
+							(!empty($params[3])) ? $controller->send_edit_comision($params[3]) : $controller->send_edit_comision(null);
 							break;
 							
 						case 'delete':
-							$controller -> eliminar_persona($params[3]);
+							(!empty($params[3])) ? $controller->delete_persona($params[3]) : $controller->delete_persona(null);
 							break;
 							
 						case 'add':
-							$controller -> agregar_persona();
+							$controller->add_persona();
+							break;
+							
+						case 'send':
+							$controller->send_form_persona();
 							break;
 							
 						default:
+							echo("Acción no reconocida.");
 							
 					}
 				}
 				break;
+			case 'comisiones':
+			 if (empty($params[2])){
+				 $controller->list_comisiones();
+			 } else {
+				 switch($params[3]){
+					 case 'edit':
+						(!empty($params[3])) ? $controller->edit_comision($params[3]) : $controller->edit_comision(null); 
+							//interfaz de edición (necesito un 3er párametro, si no está seteado, null)
+							break;
+						
+						case 'update':
+							(!empty($params[3])) ? $controller->send_edit_comision($params[3]) : $controller->send_edit_comision(null);
+							break;
+							
+						case 'delete':
+							(!empty($params[3])) ? $controller->delete_comision($params[3]) : $controller->delete_comision(null);
+							break;
+							
+						case 'add':
+							$controller->add_comision();
+							break;
+							
+						case 'send':
+							$controller->send_form_comision();
+							break;
+							
+						default:
+							echo("Acción no reconocida.");
+							
+					}
+				 }
 		}
 	}
 	
