3c26edfc1a3236c4bacb987062f49a7c8baa2c93
Blas <blas.e.martinez@live.com>
2020-10-21 16:21:23 -0300

    Script que redirige la navegación al inicio desde la página personas o personas?_nombre_ para que se cargue el header y después mostrar el contenido deseado (En caso de ingreso vía link o reload de la página).

--- includes/js/redirect.js
+++ includes/js/redirect.js
@@ -1,7 +1,8 @@
 // JavaScript Document
 /*Redirige desde las páginas de contenido a la página principal*/
 let path = window.location.pathname.substr(0, window.location.pathname.lastIndexOf('/'));
-let query = window.location.pathname.substr((window.location.pathname.lastIndexOf('/')+1),(window.location.pathname.lenght-1));
-window.location= path + "/inicio?" + query;
+let query = window.location.search;
+console.log(query);
+window.location= path + "/inicio" + query;
 /*Envía la página que estabamos viendo como search para que la pagina vuelva a mostrar lo mismo*/
 
