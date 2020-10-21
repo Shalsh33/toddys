c5968760ac8d275f480ce1d6349d9be4040b0250
Blas <blas.e.martinez@live.com>
2020-10-21 15:58:58 -0300

    Se eliminó el comentario de la manera vieja de comprobar una sesión

--- app/controller/controller.php
+++ app/controller/controller.php
@@ -4,9 +4,6 @@ class controller{
 	
 	protected function sesion(){
 		
-		/*$status = session_status();
-		
-		return ($status == PHP_SESSION_ACTIVE) ? true : false;*/
 		if (!isset($_SESSION)){
 			session_start();
 		}
