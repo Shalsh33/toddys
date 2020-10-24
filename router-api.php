72463cea86f405e06a61b5103f4b3e00fd7d777b
Blas <blas.e.martinez@live.com>
2020-10-23 23:36:06 -0300

    creado el router para la API. routeadas las acciones básicas de la api de commentarios

---
+++ router-api.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'app/api/comments.controller.php';
+require_once 'libs/Router.php';
+
+
+//Creo el Router
+$router = new Router();
+
+//Asigno las acciones
+$router->addRoute('comments','GET','comments_controller','getAll');
+$router->addRoute('comments/:user','GET','comments_controller','getGroup');
+$router->addRoute('comments/:user','POST','comments_controller','add');
+$router->addRoute('comments/:user','DELETE','comments_controller','deleteUserComments');
+$router->addRoute('comments/:user/:comment','GET','comments_controller','getOne');
+$router->addRoute('comments/:user/:comment','PUT','comments_controller','edit');
+$router->addRoute('comments/:user/:comment','delete','comments_controller','deleteComment');
+
+//Ruteo
+$router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
