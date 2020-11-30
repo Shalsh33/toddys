dea83695c9e691e9970333b2c4e189ee94263679
Blas Martínez <blas.e.martinez@live.com>
2020-11-30 13:01:19 -0300

    eliminada la llamada a la funcion js vieja. corregido los nombres de las variables que se reciben desde la api.

--- templates/inicio.tpl
+++ templates/inicio.tpl
@@ -4,4 +4,3 @@
 <article id="emergente" class="dnone"></article>
 <script type="text/javascript" src="includes/js/persona.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-<script src="includes/js/comments.js"></script>
--- templates/vue/comments.vue
+++ templates/vue/comments.vue
@@ -4,13 +4,14 @@
         <section v-for="comment in comments">
 			<div class="row">
 				<div class="col-2">
-					<p>{{ comment.user }}</p>
+					<p>{{ comment.email }}</p>
 				</div>
-				<div class="col-8">
+				<div class="col-7">
 					<p>{{ comment.content }}</p>
 				</div>
-				<div class="col-2">
-					<p>{{ comment.date }}</p>
+				<div class="col-3">
+					<p v-if="comment.edited">{{ comment.date_edited }}</p>
+					<p v-else> {{ comment.date }} </p>
 					<p v-if="comment.edited"><b>Editado</b></p>
 				</div>
 			</div>
