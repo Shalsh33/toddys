010cf7db59522777d1e19cf0e6cf62585656c170
Blas Martínez <blas.e.martinez@live.com>
2020-11-30 12:39:59 -0300

    Agregado bootstrap al proyecto. Mejorada la presentación de comentarios

--- templates/vue/comments.vue
+++ templates/vue/comments.vue
@@ -1,10 +1,21 @@
 {literal}
 
-    <ul id="comments">
-        <li v-for="comment in comments">
-            {{ comment.content }}
-        </li>
-    </ul>
+    <article id="comments">
+        <section v-for="comment in comments">
+			<div class="row">
+				<div class="col-2">
+					<p>{{ comment.user }}</p>
+				</div>
+				<div class="col-8">
+					<p>{{ comment.content }}</p>
+				</div>
+				<div class="col-2">
+					<p>{{ comment.date }}</p>
+					<p v-if="comment.edited"><b>Editado</b></p>
+				</div>
+			</div>
+        </section>
+    </article>
 
 {/literal}
 
